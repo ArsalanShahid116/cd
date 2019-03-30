0dd146b7ae201bbd72688fb61f41d288a0a5446a
ArsalanShahid116 <arsalan_shahid116@yahoo.com>
2019-03-30 23:38:10 +0000

    Finished with frontend for Login and Signup

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,10 +3,14 @@ import {Route, Switch} from 'react-router-dom';
 
 import cdEditor from './components/cdEditor';
 import NotFound from './components/NotFound';
+import Login from './containers/Login';
+import Signup from './containers/Signup';
 
 const BaseRouter = () => (
 	 <Switch>
 	     <Route exact path="/" component={cdEditor} />
+	     <Route exact path="/login" component={Login} />
+	      <Route exact path="/signup" component={Signup} />
 	     <Route component={NotFound} />
 	 </Switch>      
 );
