c2d0c5dc5f3c817ba4288f1b30ee733aec78ebe0
ArsalanShahid116 <arsalan_shahid116@yahoo.com>
2019-04-04 18:10:40 +0100

    Adding owner to user model for cdEditor

--- backend/cdEditor/models.py
+++ backend/cdEditor/models.py
@@ -2,9 +2,12 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here
 class program(models.Model):
+    owner = models.ForeignKey(User, related_name="programs",
+                        on_delete=models.CASCADE, null=True)
     language = models.CharField(max_length=30)
     title = models.CharField(max_length=70)
     description = models.CharField(max_length=120)
