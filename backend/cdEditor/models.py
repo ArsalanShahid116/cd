01e1f9b038cbda11ae61ce0f9d9cae6a83464a11
ArsalanShahid116 <arsalan_shahid116@yahoo.com>
2019-04-21 16:44:32 +0100

    Updating models to save user programs locally

--- backend/cdEditor/models.py
+++ backend/cdEditor/models.py
@@ -2,9 +2,11 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django import forms
 from django.contrib.auth.models import User
+from django.core.files.base import ContentFile
+from django.core.files.storage import default_storage
 
-# Create your models here
 class program(models.Model):
     owner = models.ForeignKey(User, related_name="programs",
                         on_delete=models.CASCADE, null=True)
@@ -15,6 +17,22 @@ class program(models.Model):
     commandLineArgs = models.TextField(blank="true") 
     output = models.TextField(blank="true")
 
+    def save(self, *args, **kwargs):
+        programLanguage = self.language
+        if (programLanguage == "python"):
+            ext = ".py"
+        elif (programLanguage == "sh"):
+            ext = ".sh"
+        elif (programLanguage == "c_cpp"):
+            ext = ".c"
+        programCode = ContentFile(self.code)
+        programOwner = self.owner
+        programTitle =  self.title
+
+        path = "UserPrograms/%s/%s%s"% (programOwner, programTitle, ext)
+        default_storage.save(path, programCode)
+
     def _str_(self):
         return self.title
+
 
